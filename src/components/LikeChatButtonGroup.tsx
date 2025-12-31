d9eec0cb8cf342807fd2d1ee969b3150377e76d2
최건희 <gunhee@choegeonhuiui-MacBookPro.local>
2026-01-01 03:36:42 +0900

    버튼 활성화 이벤트 추가 및 api기본 파일  추가

--- src/components/LikeChatButtonGroup.tsx
+++ src/components/LikeChatButtonGroup.tsx
@@ -1,10 +1,10 @@
 const LikeChatButtonGroup = () => {
   return (
     <div className="flex flex-row gap-[12px]">
-      <button className="flex items-center justify-center w-[155px] h-[52px] border rounded-[14px] px-[36px] py-[17px] font-[500] text-[16px] whitespace-nowrap">
+      <button className="flex items-center justify-center w-[155px] h-[52px] border rounded-[14px] px-[36px] py-[17px] font-[500] text-[16px] whitespace-nowrap active:bg-[#FC3367] active:text-white">
         마음에 들어요
       </button>
-      <button className="flex items-center justify-center w-[155px] h-[52px] border rounded-[14px] px-[36px] py-[17px] font-[500] text-[16px] whitespace-nowrap">
+      <button className="flex items-center justify-center w-[155px] h-[52px] border rounded-[14px] px-[36px] py-[17px] font-[500] text-[16px] whitespace-nowrap active:bg-[#FC3367] active:text-white">
         바로 대화해보기
       </button>
     </div>
