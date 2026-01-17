8ab7de682ede67cf75ad0566559643ddbb9d6341
anjaein <wodls9840@gmail.com>
2026-01-17 16:38:22 +0900

    Feat: Card.tsx 삭제 및 IdleCatd.tsx 리팩토링. 목데이터까지 수정 완료 (#38)

--- src/components/card/presets/IdleCard.tsx
+++ src/components/card/presets/IdleCard.tsx
@@ -5,34 +5,46 @@ import { CardDescription } from "../blocks/CardDescription";
 import { CardKeywords } from "../blocks/CardKeywords";
 import { CardActions } from "../actions/CardActions";
 import { RoundCardShell } from "../shell/RoundCardShell";
+import { Keyword } from "../../keyword/keyword.model";
 
-export default function IdleCard() {
+type IdleCardProps = {
+  imageUrl: string;
+  name: string;
+  age: number;
+  distance: string;
+  area: string
+  description: string;
+  keywords : Keyword[];
+};
+
+export default function IdleCard({
+  imageUrl,
+  name,
+  age,
+  distance,
+  area,
+  description,
+  keywords,
+}: IdleCardProps) {
   return (
-    <RoundCardShell imageUrl="https://picsum.photos/400/600">
+    <RoundCardShell imageUrl={imageUrl}>
       {/* 하단 가독성용 그라데이션 */}
       <div className="absolute inset-x-0 bottom-0 h-56 bg-gradient-to-t from-black/80 to-transparent" />
 
       {/* 하단 콘텐츠 영역 */}
       <div className="absolute inset-x-4 bottom-4 text-white z-10 space-y-2">
         {/* 유저 이름 */}
-        <CardUserId name="parkjun2" age={55} isVerified />
+        <CardUserId name={name} age={age} isVerified />
 
         {/* 위치 */}
-        <CardLocation distance="7km" area="분당 인근" />
+        <CardLocation distance={distance} area={area} />
 
         {/* 소개 */}
-        <CardDescription>
-          안녕하세요. 특별한 사람이라기보다는…
-        </CardDescription>
+        <CardDescription>{description}</CardDescription>
 
         {/* 키워드 */}
         <CardKeywords
-          keywords={[
-            { id: 1, label: "음악감상", category: "hobby" },
-            { id: 2, label: "근처에 사는", category: "hobby" },
-            { id: 3, label: "게임하기", category: "hobby" },
-            { id: 4, label: "조용한 성격", category: "character" },
-          ]}
+          keywords={keywords}
         />
 
         {/* 버튼 */}
