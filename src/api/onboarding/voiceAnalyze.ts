26b27b30948deeddeb65e253dd7dc4dab6746367
anjaein <wodls9840@gmail.com>
2026-01-26 23:43:46 +0900

    Revert "Feat: api 변경에 따라 voice analyse 변경(#87)"
    This reverts commit 9eb8018ebd71542953fc27342139f8d8d46d2f3e.

--- src/api/onboarding/voiceAnalyze.ts
+++ src/api/onboarding/voiceAnalyze.ts
@@ -1,24 +1,20 @@
 import { ApiSuccessResponse } from "../../types/api/api";
 import axios from "axios";
-import { IInterest, IPersonality } from "../../types/api/onboarding/onboardingDTO";
 
-interface KeywordCandidates {
-  personalities: IPersonality[];
-  interests: IInterest[];
+interface KeywordCandidate {
+  text: string;
+  score: number;
 }
-
 interface IVoiceAnalyzeRequest {
   audioUrl: string;
   language: "ko-KR";
 }
-
 interface IVoiceAnalyzeResponse {
   transcript: string;
   summary: string;
-  keywordCandidates: KeywordCandidates;
+  keywordCandidates: KeywordCandidate[];
   vibeVector: number[];
 }
-
 export const postVoiceAnalyze = async (body: IVoiceAnalyzeRequest) => {
   const lambdaUrl = import.meta.env.VITE_VOICE_LAMBDA_URL;
   const { data } = await axios.post<ApiSuccessResponse<IVoiceAnalyzeResponse>>(
