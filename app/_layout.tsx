fdc36f4dc29b98f0f87191cb7518b4aad17d0eaa
wolds9840 <wodls9840@gmail.com>
2026-05-10 02:50:34 +0900

    홈화면 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="onboarding" options={{ headerShown: false }} />
         <Stack.Screen name="profile" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen name="ideal-recording" options={{ headerShown: false }} />
         <Stack.Screen name="profile-detail" options={{ headerShown: false }} />
         <Stack.Screen name="club-create" options={{ headerShown: false }} />
         <Stack.Screen name="club-home" options={{ headerShown: false }} />
